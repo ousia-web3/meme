7d33e63663cabe6a180b11b1eee15fe08fc7736d
ousia-web3 <jaemin25.lee@gmail.com>
2025-05-03 17:55:14 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/meme/', // 👈 반드시 포함해야 함
-});
