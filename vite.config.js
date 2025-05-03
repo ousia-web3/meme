d21ff23dc77b6bf0848c7c4e185d66673e841f98
ousia-web3 <jaemin25.lee@gmail.com>
2025-05-03 18:12:02 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.',                   // ✅ 루트 디렉토리 지정
-  publicDir: 'public',         // ✅ public 폴더 명시
-  base: '/meme/',              // ✅ GitHub Pages용 base 경로
+  base: '/meme/',
   plugins: [react()],
-})
+});
