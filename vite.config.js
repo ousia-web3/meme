7667382a41247e003884d1a371c8a32bc6715638
ousia-web3 <jaemin25.lee@gmail.com>
2025-05-03 13:05:54 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/meme/',   // 꼭 저장소 이름으로 설정
-  plugins: [react()]
+  plugins: [react()],
+  base: '/meme/', // GitHub Pages 서브경로 설정
 });
